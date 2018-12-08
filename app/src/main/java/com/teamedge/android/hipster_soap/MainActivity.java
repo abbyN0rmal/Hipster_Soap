55db5124ba15cb08bb89e7bc3ad1195a5e841b1e
Student <student@Admins-MacBook-Air.local>
2018-12-08 15:39:08 -0800

    Initial commit

---
+++ app/src/main/java/com/teamedge/android/hipster_soap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teamedge.android.hipster_soap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
